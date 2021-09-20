d811611aa007a0ad50a663d33a1b9081fb15e834
haoyuwang <wanghaoyu1323@gmail.com>
2021-09-20 23:30:47 +0800

    continuing work

---
+++ src/leetcode/editor/cn/[142]环形链表 II.java
@@ -0,0 +1,118 @@
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。 
+//
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，
+//pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。 
+//
+// 说明：不允许修改给定的链表。 
+//
+// 进阶： 
+//
+// 
+// 你是否可以使用 O(1) 空间解决此题？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [3,2,0,-4], pos = 1
+//输出：返回索引为 1 的链表节点
+//解释：链表中有一个环，其尾部连接到第二个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2], pos = 0
+//输出：返回索引为 0 的链表节点
+//解释：链表中有一个环，其尾部连接到第一个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：head = [1], pos = -1
+//输出：返回 null
+//解释：链表中没有环。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围在范围 [0, 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵ 
+// pos 的值为 -1 或者链表中的一个有效索引 
+// 
+// Related Topics 哈希表 链表 双指针 👍 1179 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+//        Set<ListNode> hashSet = new HashSet<ListNode>();
+//        ListNode tail = head;
+//        int index = 0;
+//
+//        while (tail != null) {
+//            if (hashSet.contains(tail)) {
+//                return tail;
+//            } else {
+//                hashSet.add(tail);
+//                tail = tail.next;
+//            }
+//        }
+//        return null;
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+
+            if (slow == fast) {
+                ListNode result = head;
+                while (result != slow) {
+                    result = result.next;
+                    slow = slow.next;
+                }
+                return result;
+            }
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
